7d2b6a6a41a71a6faf99babfd457cad2ed1c81f8
unknown <pnp3737@gmail.com>
2020-11-17 16:39:25 +0900

    1117

--- tmp/check_ant.py
+++ tmp/check_ant.py
@@ -2,18 +2,18 @@ import os
 
 # path = 'H:/hainyoung/yolov4_3classes/cone/obj/'
 # path = 'H:/hainyoung/yolov4_3classes/cone/test/'
-# path = 'H:/hainyoung/yolov4_3classes/bollard/obj/'
-# path = 'H:/hainyoung/yolov4_3classes/bollard/test/'
+# path = 'H:/hainyoung/yolov4_3classes/1117/bollard/obj/'
+path = 'H:/hainyoung/yolov4_3classes/1117/bollard/test/'
 # path = 'H:/hainyoung/yolov4_3classes/barrel/obj/'
-path = 'H:/hainyoung/yolov4_3classes/barrel/test/'
+# path = 'H:/hainyoung/yolov4_3classes/barrel/test/'
 
 
-# out = './tmp/result/cone_obj_ant.txt'
-# out = './tmp/result/cone_test_ant.txt'
-# out = './tmp/result/bollard_obj_ant.txt'
-# out = './tmp/result/bollard_test_ant.txt'
-# out = './tmp/result/barrel_obj_ant.txt'
-out = './tmp/result/barrel_test_ant.txt'
+# out = './tmp/1117_result/cone_obj_ant.txt'
+# out = './tmp/1117_result/cone_test_ant.txt'
+# out = './tmp/1117_result/bollard_obj_ant.txt'
+out = './tmp/1117_result/bollard_test_ant.txt'
+# out = './tmp/1117_result/barrel_obj_ant.txt'
+# out = './tmp/1117_result/barrel_test_ant.txt'
 
 out_file = open(out, 'w')
 
@@ -34,7 +34,16 @@ out_file.close()
 # myfile = open('./tmp/result/bollard_obj_ant.txt') # 1145
 # myfile = open('./tmp/result/bollard_test_ant.txt') # 377
 # myfile = open('./tmp/result/barrel_obj_ant.txt') # 709
-myfile = open('./tmp/result/barrel_test_ant.txt') # 238
+# myfile = open('./tmp/result/barrel_test_ant.txt') # 238
+
+# 1117
+# myfile = open('./tmp/1117_result/cone_obj_ant.txt') # 1108
+# myfile = open('./tmp/1117_result/cone_test_ant.txt') # 174
+# myfile = open('./tmp/1117_result/bollard_obj_ant.txt') # 1305
+myfile = open('./tmp/1117_result/bollard_test_ant.txt') # 311
+# myfile = open('./tmp/1117_result/barrel_obj_ant.txt') # 709
+# myfile = open('./tmp/1117_result/barrel_test_ant.txt') # 238
+
 
 print(len(myfile.readlines()))
 

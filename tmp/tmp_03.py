7798ddd32b791af7a68f7020a6a721b405d1d687
unknown <pnp3737@gmail.com>
2020-11-23 09:31:25 +0900

    1123

--- tmp/tmp_03.py
+++ tmp/tmp_03.py
@@ -20,12 +20,23 @@ image = cv2.imread('image.jpg', -1)
 img = cv2.polylines(image, [pt2, pt3, pt4], True, red, 2)
 img = cv2.line(image, (722, 588), (722, 588), blue, 10)
 img = cv2.line(image, (573, 718), (573, 718), blue, 10)
+img = cv2.line(image, (193, 717), (193, 717), white, 1)
 
+tmp = np.array([[(145, 848)]])
 
 # roi_corners = np.array([[(8, 1014), (8,718), (860,459), (1087,452), (1084, 1015)]], dtype=np.int32)
 area_0 = np.array([[(8, 1045), (8, 718), (573, 718), (200, 1045), (8, 1045)]])
+area_2 = np.array([[(8, 718), (425, 588), (722, 588), (573, 718), (8, 718)]])
 
+print(area_0.shape)
 
-cv2.imshow('polylines', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# img = cv2.polylines(image, [area_0, area_2], True, green, 1)
+
+# if tmp in area_0:
+#     print("car")
+# else :
+#     print("nothing")
+
+# cv2.imshow('polylines', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
